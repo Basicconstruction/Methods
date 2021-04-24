d4a8ee746f34c6224fe75e964a0110a84c948269
Basicconstruction <66370519+Basicconstruction@users.noreply.github.com>
2021-04-24 19:40:29 +0800

    some change of structure

--- C++/BubbleSortTemplate.cpp
+++ C++/BubbleSortTemplate.cpp
@@ -2,8 +2,9 @@
 
 using namespace std;
 
-void reverseArray(int *list, int length) {
-    int trans;
+template<typename T>
+void reverseArrayTemplate(T *list, int length) {
+    T trans;
     for (int i = 0; i < (length - 1) / 2; i++) {
         trans = list[i];
         list[i] = list[length - 1 - i];
@@ -12,9 +13,9 @@ void reverseArray(int *list, int length) {
     return;
 }
 
-void reverseArrayOfRange(int *list, int preIndex, int endIndex) {
-    int trans;//1 2 3 4 5 6
-              //0 1 2 3 4 5
+template<typename T>
+void reverseArrayOfRangeTemplate(T *list, int preIndex, int endIndex) {
+    T trans;
     for (int i = preIndex; i < (endIndex - preIndex + 1) / 2 + preIndex; i++) {
         trans = list[i];
         list[i] = list[endIndex - i + preIndex];
@@ -23,27 +24,10 @@ void reverseArrayOfRange(int *list, int preIndex, int endIndex) {
     return;
 }
 
-void bubbleSort(int *list, int length, bool ascending = true) {
-    int trans;
-    for (int i = 0; i < length - 1; i++) {
-        for (int j = 0; j < length - 1 - i; j++) {
-            if (list[j] > list[j + 1]) {
-                trans = list[j];
-                list[j] = list[j + 1];
-                list[j + 1] = trans;
-            }
-        }
-    }
-    if (ascending) {
-        return;
-    } else {
-        reverseArray(list, length);
-    }
-}
-
-void enhancedBubbleSort(int *list, int length, bool ascending = true) {
-    //优化版冒泡排序，相比最初版的冒泡排序有一定提升
-    int trans;
+template<typename T>
+void enhancedBubbleSortTemplate(T *list, int length, bool ascending = true) {
+    //模板-优化版冒泡排序
+    T trans;
     bool sign = true;
     for (int i = 0; i < length - 1; i++) {
         for (int j = 0; j < length - 1 - i; j++) {
@@ -60,19 +44,14 @@ void enhancedBubbleSort(int *list, int length, bool ascending = true) {
         }
     }
     if (!ascending) {
-        reverseArray(list, length);
+        reverseArrayTemplate(list, length);
     }
     return;
-
 }
 
-void enhancedBubbleSortOfRange(int *list, int preIndex, int endIndex, bool ascending = true) {
-    //区域冒泡排序。
-    //目前该函数并未实现，数组越界检测，需要调用者注意，谨慎使用
-    //参数 数组 需要排序序列的初始索引  需要排序序列的结束索引
-    //默认：实现优化版冒泡排序--加入一个额外的判断
-    //默认：<= 来遍历，尤其适合约束索引
-    int trans;
+template<typename T>
+void enhancedBubbleSortOfRangeTemplate(T *list, int preIndex, int endIndex, bool ascending = true) {
+    T trans;
     bool sign = true;
     int k = 0;
     for (int i = preIndex; i <= endIndex - 1; i++) {
@@ -91,33 +70,37 @@ void enhancedBubbleSortOfRange(int *list, int preIndex, int endIndex, bool ascen
         }
     }
     if(!ascending){
-        reverseArrayOfRange(list, preIndex, endIndex);
+        reverseArrayOfRangeTemplate(list, preIndex, endIndex);
     }
     return;
 }
 
-void printArray(int *arr, int length) {
+template<typename T>
+void printArrayTemplate(T *arr, int length) {
     for (int i = 0; i < length; i++) {
         cout << arr[i];
     }
     return;
 }
 
-void printlnArray(int *arr, int length) {
+template<typename T>
+void printlnArrayTemplate(T *arr, int length) {
     for (int i = 0; i < length; i++) {
         cout << arr[i] << "\n";
     }
     return;
 }
 
-void printBlankArray(int *arr, int length) {
+template<typename T>
+void printBlankArrayTemplate(T *arr, int length) {
     for (int i = 0; i < length; i++) {
         cout << arr[i] << " ";
     }
     return;
 }
 
-void printlnBlankArrayOfRange(int *arr, int preIndex, int endIndex){
+template<typename T>
+void printlnBlankArrayOfRange(T *arr, int preIndex, int endIndex){
     for (int i = preIndex; i <= endIndex; i++) {
         cout << arr[i] << " ";
     }
@@ -125,6 +108,12 @@ void printlnBlankArrayOfRange(int *arr, int preIndex, int endIndex){
 }
 
 int main() {
-    int a[6] = {1,2,3,4,5,6};
-    printlnBlankArrayOfRange(a,2,5);
+    cout << "\n";
+    double a[10] = {1, 2, 3, 5.1, 1, 1, 5, 6.4, 8, 0.0};
+    enhancedBubbleSortTemplate(a, 10, false);
+    printBlankArrayTemplate(a, 10);
+    cout << "\n";
+    int b[6] = {1, 3, 1, 2, 4, 2};
+    enhancedBubbleSortOfRangeTemplate(b, 2, 5, false);
+    printBlankArrayTemplate(b, 6);
 }
