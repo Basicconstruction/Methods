8e09df3e0fc96d82291e3c7ffc257c166ff5af9c
Basicconstruction <66370519+Basicconstruction@users.noreply.github.com>
2021-04-25 22:33:17 +0800

    Update toSet.cpp

--- C++/toSet.cpp
+++ C++/toSet.cpp
@@ -1,6 +1,21 @@
 //
 // Created by hay boy on 2021/4/23.
 //
+#include <iostream>
+using namespace std;
+
+/**
+ * @description: 
+ * Receive an array and its length. This array is generally one more than the length of the array to be processed.
+ * After the processing of the modified function, the last bit of the array will store the effective independent bit in the array. There is no doubt that this number is greater than or equal to 1.
+ * And the length of the first valid independent bit of the array will be used to store the independent bit.
+ * @param {int} preWorkLength
+ * @return {*}
+ * @brief description(中文):
+ * 接收一个数组，以及它的长度。这个数组一般要比要处理的数组长度多1。
+ * 经过改函数的处理，该数组的最后一位将会存储该数组中有效独立位，毫无疑问这个数字大于等于1
+ * 并且该数组的前有效独立位长度将会用来存储独立位。
+ */
 void toSet(int* arr, int preWorkLength){
     bool sign = true;
     int newLength = preWorkLength;
@@ -32,4 +47,24 @@ void toSet(int* arr, int preWorkLength){
      * 改变函数特点，尽管我们传入的数组相比于原来应该长度多了一个一个长度，但是我们还是省略了将无效区段的数组元素改为其他值。
      * 尽管此函数有一定的作用，但是我们还是觉得你需要谨慎使用，尤其是需要先弄懂或会自制此或此类函数。
      * */
+}
+int main(){
+    const int m = 6;
+    const int n = 5;
+    int *a = new int(m+1);
+    int *b = new int(n+1);
+    for(int i = 0; i < m; i++){
+        cin>>a[m];
+    }
+    for(int i = 0; i < n; i++){
+        cin>>b[n];
+    }
+    toSet(a);
+    toSet(b);
+    for(int i = 0;i < a[m-1]; i++){
+        cout<<a[i]<<" ";
+    }
+    for(int i = 0;i < b[n-1]; i++){
+        cout<<a[i]<<" ";
+    }
 }
