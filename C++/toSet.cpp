c41d09d3b5524650564732fe4843e0d4d3a6e905
Basicconstruction <66370519+Basicconstruction@users.noreply.github.com>
2021-04-26 18:49:56 +0800

    Update toSet.cpp

--- C++/toSet.cpp
+++ C++/toSet.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-04-23 11:42:41
- * @LastEditTime: 2021-04-26 12:28:49
+ * @LastEditTime: 2021-04-26 18:48:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \undefinedh:\Desktop\git-Methods\Methods\C++\toSet.cpp
@@ -10,10 +10,11 @@
 // Created by hay boy on 2021/4/23.
 //
 #include <iostream>
+
 using namespace std;
 
-int localMin(int a, int b){
-    if(a < b){
+int localMin(int a, int b) {
+    if (a < b) {
         return a;
     }
     return b;
@@ -27,6 +28,146 @@ void printBlankArrayTemplate(T *arr, int length) {
     return;
 }
 
+void bigRouter() {
+    //注意这是一个未完成的方向
+
+}
+
+void localRouter(int *arr, int length, int forward, bool beGetByOtherFunction = false) {
+    //requires length % forward == 0
+    //注意这是一个未完成的方向
+    if (length % forward != 0 && !beGetByOtherFunction) {
+        throw runtime_error("error resource: localRouter(int *arr, int preIndex, int endIndex, int forward){}");
+    }
+    int localPreIndex;
+    int localEndIndex;
+    int trans;
+    for (int i = 1; i <= forward; i++) {
+        localPreIndex = i - 1;
+        localEndIndex = length - forward + localPreIndex;
+        trans = arr[localPreIndex];
+        arr[localPreIndex] = arr[localEndIndex];
+        arr[localEndIndex] = trans;
+    }
+}
+
+//交换数组局部的 前n位与后n位
+void localRouterOfRange(int *arr, int preIndex, int endIndex, int forward, bool beGetByOtherFunction = false);
+
+void elementsMoveOneStep(int *arr, int length, bool pushForward = true) {
+    //对数组进行整体移位，移位长度为1，pushForward为true时向前移位，否则向后移位
+    if (pushForward) {
+        int first;
+        first = arr[0];
+        for (int i = 0; i < length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[length - 1] = first;
+    } else {
+        int end;
+        end = arr[length - 1];
+        for (int i = 0; i < length - 1; i++) {
+            arr[i + 1] = arr[i];
+        }
+        arr[0] = end;
+    }
+
+}
+
+void elementsMoveOneStep(int *arr, int preIndex, int endIndex, bool pushForward = true) {
+    //对数组进行整体移位，移位长度为1，pushForward为true时向前移位，否则向后移位
+    if (pushForward) {
+        int first;
+        first = arr[preIndex];
+        for (int i = preIndex; i <= endIndex - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[endIndex] = first;
+    } else {
+        int end;
+        //2 3 4 5 6
+        //6 2 3 4 5
+        end = arr[endIndex];
+        for (int i = endIndex; i > preIndex; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[preIndex] = end;
+    }
+
+}
+
+void elementsMoveForward(int *arr, int length, int forward = 1, bool pushForward = true) {
+    //forward >= 1 否则将会无意义
+    if (forward < 1) {
+        return;
+    }
+    int preIndex;
+    int endIndex;
+    int trans;
+    localRouter(arr, length, forward, true);
+    //debug
+//    printBlankArrayTemplate(arr, length);
+//    cout<<"\n";
+    for (int j = forward; j < length - forward;) {
+        if (length - forward - j >= forward) {
+            for (int k = 1; k <= forward; k++) {
+                trans = arr[j - forward + k - 1];
+                arr[j - forward + k - 1] = arr[j + k - 1];
+                arr[j + k - 1] = trans;
+            }
+            j = j + forward;
+            //debug
+//            cout<<"$j"<<j<<"\n";
+//            printBlankArrayTemplate(arr, length);
+//            cout<<"\n";
+        } else {
+            int nl = length - forward - j;
+            for (int i = 0; i < nl; i++) {
+                elementsMoveOneStep(arr, j - forward, length - forward - 1, !pushForward);
+            }
+            j = j + 4;
+            //debug
+//            printBlankArrayTemplate(arr, length);
+//            cout<<"\n";
+        }
+
+    }
+}
+
+void localUnequalLengthRouter(int *arr, int preIndex, int endIndex, int transLength);
+
+void toSetNormal(int *arr, int length, int transLength = 1, bool pushForward = true) {
+    if (length / transLength == 2) {
+        localRouter(arr, length, transLength);
+        return;
+    }
+    if(transLength >= 2 && length / transLength >= 3){
+        elementsMoveForward(arr, length, transLength, true);
+        return;
+    }
+    if (transLength > length / 2) {
+        for (int i = 0; i < length - transLength; i++) {
+            elementsMoveOneStep(arr, length, !pushForward);
+        }
+    } else {
+        for (int i = 0; i < transLength; i++) {
+            elementsMoveOneStep(arr, length, pushForward);
+        }
+    }
+}
+
+void toSet(int *arr, int l);
+
+int main() {
+
+    int fg[19] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+    toSetNormal(fg, 19, 4, true);
+    printBlankArrayTemplate(fg, 19);
+
+
+}
+
+
 /**
  * @description:
  * Receive an array and its length. This array is generally one more than the length of the array to be processed.
@@ -39,143 +180,49 @@ void printBlankArrayTemplate(T *arr, int length) {
  * 经过改函数的处理，该数组的最后一位将会存储该数组中有效独立位，毫无疑问这个数字大于等于1
  * 并且该数组的前有效独立位长度将会用来存储独立位。
  */
-void toSet(int* arr, int l){
-    //在运行时经常会异常退出，debug时可以正常运行
+void toSet(int *arr, int l) {
+    //借助动态数组实现，运行时会出现异常
     bool sign = true;
     int nl = l;
-    int* transArr = new int(nl);//nl = l = 6
+    int *transArr = new int(nl);//nl = l = 6
     transArr[nl - 1] = 0;//transArr[newLength -1] = effectiveLengthOf "transArr"
-    for(int i = 0; i < l - 1;i++){
-        for(int j = 0;j < transArr[nl - 1]; j++){
-            if(arr[i] == transArr[j]){
+    for (int i = 0; i < l - 1; i++) {
+        for (int j = 0; j < transArr[nl - 1]; j++) {
+            if (arr[i] == transArr[j]) {
                 sign = false;
             }
         }
-        if(sign){
+        if (sign) {
             transArr[transArr[nl - 1]] = arr[i];
-            transArr[nl - 1] = transArr[nl -1] + 1;
+            transArr[nl - 1] = transArr[nl - 1] + 1;
             sign = true;
         }
     }
-    for(int k = 0; k < transArr[nl - 1];k++){
+    for (int k = 0; k < transArr[nl - 1]; k++) {
         arr[k] = transArr[k];
     }
-    arr[l - 1] =  transArr[nl - 1];//arr列表最后一位放置有效长度，不是有效索引哟
-}
-void elementsMoveForward(int *arr, int length, int forward = 1){
-    //forward >= 1 否则将会无意义
-    //注意这是一个未完成的方向
-    if(forward < 1){
-        return;
-    }
-    int preIndex;
-    int endIndex;
-    int trans;
-    for(int i = 1; i <= forward;i++){
-        preIndex = i - 1;
-        endIndex = length - forward + preIndex;
-        trans = arr[preIndex];
-        arr[preIndex] = arr[endIndex];
-        arr[endIndex] = trans;
-    }
-    for(int j = forward;j<length - forward;){
-        /*1 2 3 4 5 6 7
-         * 0 1 2 3 4 5 6
-         * 5 6 7 4 1 2 3
-         * j = 3 j < 4
-         * k = 1 k <= min(3,4)=3
-         *
-         * */
-        for(int k = 1; k <= localMin(forward, length - forward - j);k++){
-            trans = arr[j - forward + k - 1];
-            arr[j - forward + k - 1] = arr[j + k - 1];
-            arr[j + k - 1] = trans;
-        }
-        j = j+forward;
-    }
+    arr[l - 1] = transArr[nl - 1];//arr列表最后一位放置有效长度，不是有效索引哟
 }
 
-void bigRouter(){
-    //注意这是一个未完成的方向
-
-}
-
-void localRouter(int *arr, int length, int forward){
-    //requires length % forward == 0
-    //注意这是一个未完成的方向
-    int preIndex;
-    int endIndex;
-    int trans;
-    for(int i = 1; i <= forward;i++){
-        preIndex = i - 1;
-        endIndex = length - forward + preIndex;
-        trans = arr[preIndex];
-        arr[preIndex] = arr[endIndex];
-        arr[endIndex] = trans;
-    }
-}
-
-//交换数组局部的 前n位与后n位
-void localRouter(int *arr, int preIndex, int endIndex, int forward){
-    //注意这是一个未完成的方向
-    //requires length % forward == 0
+void localRouterOfRange(int *arr, int preIndex, int endIndex, int forward, bool beGetByOtherFunction) {
+//requires length % forward == 0
+//注意这是一个未完成的方向
     int trans;
     int localArrLength = endIndex - preIndex + 1;
-//    if(localArrLength % forward != 0){
-//        throw runtime_error("error resource: localRouter(int *arr, int preIndex, int endIndex, int forward){}");
-//    }
+    if (localArrLength % forward != 0 && beGetByOtherFunction) {
+        throw runtime_error("error resource: localRouter(int *arr, int preIndex, int endIndex, int forward){}");
+    }
     int localPreIndex;
     int localEndIndex;
-    for(int i = 1; i <= forward;i++){
-        localPreIndex = i - 1;
+    for (int i = 1; i <= forward; i++) {
+        localPreIndex = preIndex + i - 1;
         localEndIndex = localArrLength - forward + localPreIndex;
         trans = arr[localPreIndex];
         arr[localPreIndex] = arr[localEndIndex];
         arr[localEndIndex] = trans;
     }
 }
-
-void elementsMoveOneStep(int *arr, int length, bool pushForward = true){
-    //对数组进行整体移位，移位长度为1，pushForward为true时向前移位，否则向后移位
-    if(pushForward){
-        int first;
-        first = arr[0];
-        for(int i = 0; i < length - 1; i++){
-            arr[i] = arr[i + 1];
-        }
-        arr[length - 1] = first;
-    }else{
-        int end;
-        end = arr[length - 1];
-        for(int i = 0; i < length - 1; i++){
-            arr[i+1] = arr[i];
-        }
-        arr[0] = end;
-    }
-
-}
-
-//数组局部  交换数组总cLength = n + x
-//         原交换部分长度tLength = n
-//比如: 1 2 3 4 5
-/*
- * cLength = 5
- * tLength = n = 3
- * x = 2
- * 交换后: 4 5 1 2 3
- * 处理策略: 即时更改 当前交换部分长度
- * 如本次交换  nLength = 2 = cLength - 3
- * 交换 1 2 和  4 5
- * 生成 4 5 3 1 2
- * n当前交换长度 nLength = tLength - nLength = 1
- * 再将数组 向前移动一位即可
- * */
-// 0 < x < n
-/*
- * length < 2n
- *
- * */
-void localUnequalLengthRouter(int *arr, int preIndex, int endIndex ,int transLength){
+void localUnequalLengthRouter(int *arr, int preIndex, int endIndex, int transLength) {
     //
     int cLength = endIndex - preIndex + 1;//交换数组总长度
     int sectionLength = cLength;//现在交换数组总长度
@@ -187,9 +234,9 @@ void localUnequalLengthRouter(int *arr, int preIndex, int endIndex ,int transLen
     int localEndIndex;
     bool sign = false;
     int i;
-    while(!sign){
+    while (!sign) {
         sectionLength = sectionEndIndex - sectionPreIndex + 1;
-        for(i = sectionPreIndex;i + nTLength <= sectionEndIndex - nTLength;){
+        for (i = sectionPreIndex; i + nTLength <= sectionEndIndex - nTLength;) {
             localPreIndex = i;
             localEndIndex = sectionEndIndex;
             localRouter(arr, localPreIndex, localEndIndex, nTLength);
@@ -199,78 +246,4 @@ void localUnequalLengthRouter(int *arr, int preIndex, int endIndex ,int transLen
         sectionEndIndex = sectionPreIndex + (sectionLength % nTLength) - 1;
         sign = true;
     }
-}
-
-void toSet2(int* arr, int l){
-    //self tackle
-    bool sign = true;
-    int effectL = 0;
-    arr[l-1] = effectL;
-    for(int i = 1; i < l;i++){
-        for(int j = 0;j < i; j++){
-            if(arr[i] == arr[j]){
-
-            }
-        }
-    }
-
-}
-//1 2 3 4 5 6
-//0 1 2 3 4 5
-//5 6 3 4 1 2
-/**
- j = 2 j < 6
- k = 1 k <= min(2,3) = 2
- 3 4 5 6 1 2
-j = 4 j < 6
- k = 1 k <=
- */
-
- void toSetNormal(int *arr, int length, int transLength = 1, bool pushForward = true){
-    if(transLength > length / 2){
-        for(int i = 0; i < length - transLength; i++){
-            elementsMoveOneStep(arr, length, !pushForward);
-        }
-    }else{
-        for(int i = 0; i < transLength; i++){
-            elementsMoveOneStep(arr, length, pushForward);
-        }
-    }
- }
-int main(){
-    int fg[14] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-    int f[14] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-//    localUnequalLengthRouter(fg,0,13,4);
-    toSetNormal(fg, 14, 5, true);
-    toSetNormal(f,14,9,false);
-    printBlankArrayTemplate(fg,14);
-    cout<<"\n";
-    printBlankArrayTemplate(f,14);
-//    int c[6] = {1,2,3,4,5,6};
-//    elementsMoveForward(c,6,2);
-//    printBlankArrayTemplate(c,6);
-//    cout<<"\n";
-//    int d[7] = {1,2,3,4,5,6,7};
-//    elementsMoveForward(d,7,3);
-//    printBlankArrayTemplate(d,7);
-//    cout<<"\n";
-//    int f[8] = {1,2,3,4,5,6,7,8};
-//    localRouter(f,0,5,3);
-//    printBlankArrayTemplate(f,8);
-//    cout<<"\n";
-//    int p[4] = {1,2,3,4};
-//    elementsMoveOneStep(p,4);
-//    printBlankArrayTemplate(p,4);
-//    elementsMoveForward(f,8,2);
-//    printBlankArrayTemplate(f,8);
-//    int a[6] = {1,2,3,4,1,2};//0 1 2 3 4 5
-//    int b[5] = {1,2,3,1,2};// 0 1 2 3 4
-//    toSet(a, 6);
-//    toSet(b, 5);
-//    for(int j = 0;j < a[5]; j++){
-//        cout<<a[j]<<" ";
-//    }
-//    for(int k = 0;k < b[4]; k++){
-//        cout<<b[k]<<" ";
-//    }
 }
