849b1aa75b06177b2486beafc01e007a778ee07c
Basicconstruction <66370519+Basicconstruction@users.noreply.github.com>
2021-05-01 22:27:05 +0800

    push 2021.5.1 last push

--- KotlinGit/B/src/Hero.kt
+++ KotlinGit/B/src/Hero.kt
@@ -1,4 +1,41 @@
+import kotlin.properties.Delegates
+fun <T> T.easyPrint():T{
+    println(this)
+    return this
+}
+fun add(a:Int, b:Int):Int{
+    return a+b
+}
+
+class SpellBook{
+    @JvmField
+    val spells = listOf("Magic Ms. L", "Lay on Hans")
+}
+
+@JvmOverloads
+fun handOverFood(leftHand:String = "berriers", rightHand:String = "beef"){
+    println("Mmmm, you hand some decilious food $leftHand and $rightHand")
+}
 fun main(args:Array<String>){
-    val J1:Jhave = Jhave()
-    println(J1.utterGreeting())
+//    val J1:Jhave = Jhave()
+//    println(J1.utterGreeting())
+//    println(J1.determineFriendshipLevel()?.toUpperCase() ?: "Null, I don't know what is friendship!")
+//    println(J1.javaClass)
+//    J1.hitPoints = 1000
+//    println(J1.hitPoints)
+//    println(J1.a)
+    add(2,3).easyPrint()
+//    3.let{
+//        "hello"
+//    }.easyPrint()
+//    2.also{
+//        Jhave()
+//    }.easyPrint()
+//    2.run{
+//        "hello world"
+//        Jhave()
+//    }.easyPrint()
+//    with(3){
+//        this + 1
+//    }.easyPrint()
 }
--- KotlinGit/B/src/Jhave.java
+++ KotlinGit/B/src/Jhave.java
@@ -1,5 +1,34 @@
+import org.jetbrains.annotations.Nullable;
+
 public class Jhave {
+    private int HitPoints = 100;
+    private int a = 10;
     public String utterGreeting(){
         return "black";
     }
+    @Nullable
+    public String determineFriendshipLevel(){
+        return null;
+    }
+    public void setHitPoints(int value){
+        this.HitPoints = value * 10;
+    }
+    public int getHitPoints(){
+        return this.HitPoints * 10;
+    }
+    public void setA(int value){
+        this.a = value * 10;
+    }
+    public int getA(){
+        return this.a * 10;
+    }
+    public static void main(String[] args){
+        System.out.println(HeroKt.add(3,1));
+        HeroKt.handOverFood();
+        SpellBook h = new SpellBook();
+        for(String spell : h.spells){
+            System.out.println(spell);
+        }
+    }
+
 }
