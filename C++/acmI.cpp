ca09f02f4f21caec08ccd3ff74aecb35c4d81eab
Basicconstruction <66370519+Basicconstruction@users.noreply.github.com>
2021-05-09 21:57:37 +0800

    Update acmI.cpp

--- C++/acmI.cpp
+++ C++/acmI.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
-void printYes(int a, int b, int c){
-    if((a+b>c)&&(a+c>b)&&(b+c>a)&&a>0&&b>0&&c>0){
-        cout<<"YES";
+template <typename T,typename T2,typename T3>
+void printYes(T a, T2 b, T3 c){
+    if((a+b>c)&&(a+c>b)&&(b+c>a)){
+        cout<<"YES\n";
     }else{
-        cout<<"NO";
+        cout<<"NO\n";
     }
 }
 int main() {
     int lines;
     cin>>lines;
-    int a,b,c;
+    auto a = 0.0f;
+    auto b = 0.0f;
+    auto c = 0.0f;
     for(int i = 0; i< lines;i++){
         cin>>a>>b>>c;
         printYes(a,b,c);
